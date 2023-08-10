6bb85b7d1231172f0cdb302eea49d71fd9baa0dc
AhmetED07 <85395314+AhmetED07@users.noreply.github.com>
2023-08-10 15:25:38 +0300

    Add files via upload

---
+++ komutlar/G-Komut.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args, level) => {
+  const toplamkomut = new Discord.MessageEmbed()
+
+    .setTitle(``)
+    .setAuthor(`Muharrem Belinay  | Discord Bot Toplam Komut Sayısı`)
+    .setDescription(
+      `✅ **Muharrem Belinay  Toplam**  \`` +
+        client.commands.size +
+        `\` **Komut Vardır!**`
+    )
+    .setColor("#00ff00")
+    .setTimestamp()
+    .setFooter(`Muharrem Belinay`, client.user.avatarURL());
+
+  return message.channel.send(toplamkomut);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "komutlar",
+  description: "Toplam Komut",
+  usage: "toplamkomut"
+};
